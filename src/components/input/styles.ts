7f5dbaf2ba6e1dfe321e6e3ad0846872c7a883d4
Jorge <jorgews.dev@gmail.com>
2021-02-25 07:58:05 -0300

    feat(Tooltip): 🎸 - Create Tooltip component and implement it on Input component.

--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -1,8 +1,10 @@
 import styled, { css } from 'styled-components';
+import Tooltip from '../tooltip';
 
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -20,6 +22,13 @@ export const Container = styled.div<ContainerProps>`
   & + div {
     margin-top: 8px;
   }
+
+  ${props =>
+    props.isErrored &&
+    css`
+      border: 2px solid #c53030;
+      color: #c53030;
+    `}
 
   ${props =>
     props.isFocused === true &&
@@ -55,5 +64,23 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
