e5b7995698825822c06580660e09027173edfec2
Jorge <jorgews.dev@gmail.com>
2021-02-24 21:52:50 -0300

    feat(FormSignup): 🎸 - Add the unform lib to control the form.

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,34 +1,40 @@
 import React from 'react';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import { Container, Content, Background } from './styles';
 import Logo from '../../assets/logo.svg';
 import Input from '../../components/input';
 import Button from '../../components/button';
 
-const Signup: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={Logo} alt="GoBarber" />
-      <form>
-        <h1>Faça seu casdastro</h1>
-        <Input name="nome" icon={FiUser} placeholder="Nome" />
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Password"
-        />
-        <Button type="submit">Cadastrar</Button>
-      </form>
+const Signup: React.FC = () => {
+  const handleSubmitForm = (data: Object): void => {
+    console.log(data);
+  };
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={Logo} alt="GoBarber" />
+        <Form onSubmit={handleSubmitForm}>
+          <h1>Faça seu casdastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Password"
+          />
+          <Button type="submit">Cadastrar</Button>
+        </Form>
 
-      <a href="login">
-        <FiArrowLeft />
-        Voltar para login
-      </a>
-    </Content>
-  </Container>
-);
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para login
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default Signup;
