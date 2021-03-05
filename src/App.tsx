dd4e86e94418d2b68a4de5a93b6450c0ff9b28e4
Jorge <jorgews.dev@gmail.com>
2021-03-05 05:12:37 -0300

    feat: 🎸 - Create Signout method and change the name of folder 'context' to 'hooks'.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/signin';
 // import SignUp from './pages/signup';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -15,6 +15,7 @@ interface AuthContextData {
   user: object;
   // eslint-disable-next-line no-unused-vars
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -28,6 +29,7 @@ const AuthProvider: React.FC = ({ children }) => {
 
     return {} as AuthState;
   });
+
   const signIn = useCallback(async ({ email, password }) => {
     try {
       const response = await api.post('sessions', {
@@ -46,8 +48,15 @@ const AuthProvider: React.FC = ({ children }) => {
     }
   }, []);
 
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthState);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
