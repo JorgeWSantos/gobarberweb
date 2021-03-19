0607ec2fe8db8b9f116aa52522a49aebaa1998ca
Jorge <jorgews.dev@gmail.com>
2021-03-18 23:09:03 -0300

    refactor: 💡- Change the file name.

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,71 @@
+import React, { createContext, useCallback, useContext, useState } from 'react';
+import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user: Object;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: object;
+  // eslint-disable-next-line no-unused-vars
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    if (token && user) return { token, user: JSON.parse(user) };
+
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    });
+
+    const { user, token } = response.data;
+
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    setData({ token, user });
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('Error');
+  }
+
+  return context;
+}
+
+export { useAuth, AuthProvider };
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -4,7 +4,7 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 import { Container, Content, Background } from './styles';
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
 import Logo from '../../assets/logo.svg';
 import Input from '../../components/input';
 import Button from '../../components/button';
